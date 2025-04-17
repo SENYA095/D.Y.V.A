3c54a763ee6c5f03c7e16f2f80f911d28fb027a0
SENYA095 <korolarsen404@gmail.com>
2025-04-17 22:15:27 +0300

    Fresh new version

--- scripts/slider.js
+++ scripts/slider.js
@@ -5,43 +5,43 @@ const totalSlides = slides.length;
 let currentIndex = 0;
 
 function updateSlider() {
-		slider.style.transform = `translateX(-${currentIndex * 444}px)`;
+	slider.style.transform = `translateX(-${currentIndex * 444}px)`;
 }
 
 document.getElementById('next').addEventListener('click', () => {
 	const slidesPerView = 2;
-		if (currentIndex < totalSlides - slidesPerView) {
-				currentIndex++;
-		} else {
-				currentIndex = 0; // Повернення до першого слайду
-		}
-		updateSlider();
+	if (currentIndex < totalSlides - slidesPerView) {
+		currentIndex++;
+	} else {
+		currentIndex = 0; // Повернення до першого слайду
+	}
+	updateSlider();
 });
 
 document.getElementById('prev').addEventListener('click', () => {
-		if (currentIndex > 0) {
-				currentIndex--;
-		} else {
-				currentIndex = totalSlides - 2; // Перехід до останнього слайду
-		}
-		updateSlider();
+	if (currentIndex > 0) {
+		currentIndex--;
+	} else {
+		currentIndex = totalSlides - 2; // Перехід до останнього слайду
+	}
+	updateSlider();
 });
 
 document.getElementById('next-btn').addEventListener('click', () => {
 	const slidesPerView = 2;
-		if (currentIndex < totalSlides - slidesPerView) {
-				currentIndex++;
-		} else {
-				currentIndex = 0; // Повернення до першого слайду
-		}
-		updateSlider();
+	if (currentIndex < totalSlides - slidesPerView) {
+		currentIndex++;
+	} else {
+		currentIndex = 0; // Повернення до першого слайду
+	}
+	updateSlider();
 });
 
 document.getElementById('prev-btn').addEventListener('click', () => {
-		if (currentIndex > 0) {
-				currentIndex--;
-		} else {
-				currentIndex = totalSlides - 2; // Перехід до останнього слайду
-		}
-		updateSlider();
+	if (currentIndex > 0) {
+		currentIndex--;
+	} else {
+		currentIndex = totalSlides - 2; // Перехід до останнього слайду
+	}
+	updateSlider();
 });
